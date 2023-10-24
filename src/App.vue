331e57643b4ce4a6655b95a446c7db0770979cfd
kun99 <kuns.km@gmail.com>
2023-10-25 01:07:57 +0700

    Plays video that matches video id

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,61 @@
-<script setup></script>
-
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      video: null,
+      title: ""
+    }
+  },
+  mounted() {
+    const urlParams = new URLSearchParams(window.location.search);
+    axios
+      .post("http://localhost:5001/video", {
+        id: urlParams.get('vid'),
+      })
+      .then((response) => {
+        this.video = response.data.video;
+        this.title = response.data.title;
+        console.log(this.video)
+      })
+      .catch((error) => {
+        console.error("Couldn't fetch video:", error);
+      });
+  },
+};
+</script>
 <template>
-  <div class="container">
-    <video controls>
-      <source src="" type="video" />
-    </video>
+  <div class="video-container">
+    <div class="container" v-if="video">
+      <video controls>
+        <source :src="video" />
+      </video>
+    </div>
+    <div class="title">
+      <h2>{{ this.title }}</h2>
+    </div>
   </div>
   <div class="arrow left-arrow" @click="navigate('left')">&#8249;</div>
   <div class="arrow right-arrow" @click="navigate('right')">&#8250;</div>
 </template>
 
 <style>
+.video-container {
+  display: flex;
+  justify-content: center;
+}
 .container {
-  width: 500px;
+  width: 30%;
   height: 850px;
-  margin: 0 auto;
 }
-
-video {
+.container video {
   width: 100%;
-  height: 100%;
+  height: 90%;
 }
-
+.title {
+  position: absolute;
+  color: black;
+}
 .arrow {
   position: fixed;
   top: 50%;
@@ -30,11 +64,9 @@ video {
   padding: 10px 20px;
   cursor: pointer;
 }
-
 .left-arrow {
   left: 20px;
 }
-
 .right-arrow {
   right: 20px;
 }
